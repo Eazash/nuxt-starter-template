8eb5bfdea298b2ead8d263847f0171e0a88ec737
Ezira Ashenafi <eazash22@gmail.com>
2024-10-21 15:58:19 +0300

    feat: initiliaze folder structure and core modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-10-21',
+  devtools: { enabled: true },
+  modules: [
+    '@nuxt/fonts',
+    '@nuxtjs/tailwindcss',
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/image'
+  ],
+  eslint: {
+    config : {
+      stylistic: false,
+      standalone: false
+    }
+  }
 })
