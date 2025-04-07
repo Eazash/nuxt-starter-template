7cd9729603cbb05b6c5eef31adfca85c03d30cd7
Ezira Ashenafi <eazash22@gmail.com>
2025-04-07 17:02:38 +0300

    chore: add @antfu/eslint-config with slight tweaks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,35 @@
 // @ts-check
+import antfu from '@antfu/eslint-config'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
-  // Your custom configs here
+  antfu({
+    vue: true,
+  }, {
+    files: ['**/*.vue'],
+    rules: {
+      'import/exports-last': 'off',
+      'vue/block-lang': ['error', {
+        script: {
+          lang: 'ts',
+        },
+      }],
+      'vue/define-props-declaration': ['error', 'type-based'],
+      'vue/define-emits-declaration': ['error', 'type-literal'],
+    },
+  }, {
+    files: ['**/*.ts', '**/*.vue'],
+    rules: {
+      'ts/consistent-type-definitions': ['warn', 'type'],
+      'ts/no-explicit-any': 'error',
+      'node/prefer-global/process': ['error', 'always'],
+      'jsdoc/no-multi-asterisks': ['warn', { allowWhitespace: true }],
+    },
+  }, {
+    rules: {
+      'curly': ['error', 'all'],
+      'style/max-statements-per-line': ['error', { max: 1 }],
+      'style/brace-style': ['error', '1tbs'],
+    },
+  }),
 )
